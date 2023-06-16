b3da0d05512d1a48e7c87fbc24f9a48e2f93238d
gur_u <gur_u@DESKTOP-8FCU5LA>
2023-06-16 13:15:49 +0500

    Добавьте файлы проекта.

---
+++ Person.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Marriornot
+{
+    public class Person
+    {
+        public bool marry_or_not { get; set; }
+        public string name { get; set; }
+    }
+
+    public class Marriage
+    {
+        public string Partner1 { get; set; }
+        public string Partner2 { get; set; }
+
+        public Marriage(string partner1, string partner2)
+        {
+            Partner1 = partner1;
+            Partner2 = partner2;
+        }
+    }
+
+    public class MarriageHelper
+    {
+        public static Marriage CreateMarriage(string partner1, string partner2)
+        {
+            return new Marriage(partner1, partner2);
+        }
+
+        public static Marriage CreateMarriageWithPerson(string partner1, Person person)
+        {
+            if (person.marry_or_not)
+            {
+                return new Marriage(partner1, person.name);
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using Marriornot;
+using System;
+
+namespace Marriorno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person { name = "John", marry_or_not = false };
+
+            Marriage marriage1 = MarriageHelper.CreateMarriageWithPerson("Jane", person1);
+
+            if (marriage1 == null)
+            {
+                Console.WriteLine("The person is not married.");
+            }
+            else
+            {
+                Console.WriteLine("{0} is married to {1}.", marriage1.Partner1, marriage1.Partner2);
+            }
+        }
+    }
+}
